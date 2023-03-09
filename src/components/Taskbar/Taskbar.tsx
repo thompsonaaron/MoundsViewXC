6c42e33517bbbda6bee17129a7212eae59cb9393
thompsonaaron <59658614+thompsonaaron@users.noreply.github.com>
2023-03-09 11:04:58 -0600

    updated links

--- src/components/Taskbar/Taskbar.tsx
+++ src/components/Taskbar/Taskbar.tsx
@@ -25,22 +25,22 @@ const Taskbar = () => {
       </div>
       {showNav && (
         <div class="flex flex-col fixed bottom-0 mb-14 transition-all h-fit w-full bg-white border-t-4 border-green-700">
-          <a href="/" class="m-6 text-gray-400 hover:text-black">
+          <a href="/moundsviewxc/" class="m-6 text-gray-400 hover:text-black">
             HOME
           </a>
-          <a href="/schedule" class="m-6 text-gray-400 hover:text-black">
+          <a href="/moundsviewxc/schedule" class="m-6 text-gray-400 hover:text-black">
             SCHEDULE
           </a>
-          <a href="/results" class="m-6 text-gray-400 hover:text-black">
+          <a href="/moundsviewxc/results" class="m-6 text-gray-400 hover:text-black">
             RESULTS
           </a>
-          <a href="/coaches" class="m-6 text-gray-400 hover:text-black">
+          <a href="/moundsviewxc/coaches" class="m-6 text-gray-400 hover:text-black">
             COACHES
           </a>
           {/* <a href="/captains" class="m-6 text-gray-400 hover:text-black">
             CAPTAINS
           </a> */}
-          <a href="/media" class="m-6 text-gray-400 hover:text-black">
+          <a href="/moundsviewxc/media" class="m-6 text-gray-400 hover:text-black">
             MEDIA
           </a>
         </div>
