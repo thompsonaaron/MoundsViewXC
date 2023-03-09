6759b7f4813745f1fbc86a8a250390b1e4d63b09
thompsonaaron <59658614+thompsonaaron@users.noreply.github.com>
2023-03-09 10:59:06 -0600

    astro and deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind()]
+  integrations: [preact(), tailwind()],
+  site: "https://thompsonaaron.github.io",
+  base: "/MoundsViewXC",
 });
