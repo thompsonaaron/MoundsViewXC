03e36523da83fd7b62231c956f4ebe9bfa09671e
thompsonaaron <59658614+thompsonaaron@users.noreply.github.com>
2023-03-11 22:21:59 -0600

    added media dropdown

---
+++ src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,76 @@
+import clsx from "clsx";
+import { useState } from "preact/hooks";
+
+const MediaDropdown = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	return (
+		<div class={"flex flex-col"}>
+			<div class="flex relative cursor-pointer m-0 p-0 items-center text-gray-400 hover:text-black active:text-black">
+				<span
+					class="mx-4 md:mx-6 z-50"
+					onClick={() => setIsOpen((prevValue) => !prevValue)}>
+					MEDIA
+					<svg
+						viewBox="0 0 24 24"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+						class={clsx(
+							"transition-all duration-100 ease-in-out self-right inline-block h-4 mx-1 fill-red-200 hover:fill-red-700",
+							{
+								"-rotate-90 md:rotate-180": isOpen,
+								"rotate-0": !isOpen,
+							}
+						)}>
+						<g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+						<g
+							id="SVGRepo_tracerCarrier"
+							stroke-linecap="round"
+							stroke-linejoin="round"></g>
+						<g id="SVGRepo_iconCarrier">
+							<path
+								fill="currentColor"
+								d="M5.16108 10.0731C4.45387 9.2649 5.02785 8 6.1018 8H17.898C18.972 8 19.5459 9.2649 18.8388 10.0731L13.3169 16.3838C12.6197 17.1806 11.3801 17.1806 10.6829 16.3838L5.16108 10.0731ZM6.65274 9.5L11.8118 15.396C11.9114 15.5099 12.0885 15.5099 12.1881 15.396L17.3471 9.5H6.65274Z"></path>
+						</g>
+					</svg>
+				</span>
+			</div>
+			<div class="relative text-left left-2">
+				<ul
+					class={clsx(
+						"absolute m-0 bg-white w-full px-4 py-2 -top-24 md:-top-8 left-12 md:right-0 flex flex-col transition-all duration-1000 ease-in-out",
+						{
+							"static flex z-100bg-none opacity-100 z-50": isOpen,
+							"translate-x-12 md:translate-y-10 md:translate-x-0": isOpen,
+							"duration-1000 ease-in-out translate-x-0 md:translate-y-0 opacity-0 z-0":
+								!isOpen,
+						}
+					)}>
+					<li class="pl-1 text-gray-400 hover:text-black">
+						<a
+							href="http://moundsviewboyscrosscountry.shutterfly.com/"
+							target="_blank">
+							XC Photos
+						</a>
+					</li>
+					<li class="pl-1 text-gray-400 hover:text-black">
+						<a
+							href="https://moundsviewboystrack2017.shutterfly.com/#"
+							target="_blank">
+							TF Photos
+						</a>
+					</li>
+					<li class="pl-1 text-gray-400 hover:text-black">
+						<a
+							href="https://www.youtube.com/channel/UCg_Z3H_nyqWZvTqml7jGIwQ?view_as=subscriber"
+							target="_blank">
+							Videos
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
+};
+
+export default MediaDropdown;
