ffb1a4d025be7cd74e39412476ca8353396315c8
thompsonaaron <59658614+thompsonaaron@users.noreply.github.com>
2023-03-15 14:56:46 -0500

    updates links for prod, coaches page with Pete

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,65 +1,68 @@
-import { useEffect, useState } from "preact/hooks";
+import { useEffect, useState } from 'preact/hooks'
 
-import Alexandria from "../../assets/img-4530_orig.jpg";
-import Hugs from "../../assets/img-7128_orig.jpg";
-import Rosemount2015 from "../../assets/img-2015-rosemount-start.jpg";
-import SEC2015 from "../../assets/img-2015-cc-conference-walking.jpg";
-import SectionsFourPeat from "../../assets/img-8187_orig.jpg";
-import SectionsTeam from "../../assets/img-3485_orig.jpg";
-import clsx from "clsx";
+import Alexandria from '../../assets/img-4530_orig.jpg'
+import Hugs from '../../assets/img-7128_orig.jpg'
+import Rosemount2015 from '../../assets/img-2015-rosemount-start.jpg'
+import SEC2015 from '../../assets/img-2015-cc-conference-walking.jpg'
+import SectionsFourPeat from '../../assets/img-8187_orig.jpg'
+import SectionsTeam from '../../assets/img-3485_orig.jpg'
+import clsx from 'clsx'
 
-const imageList = [Rosemount2015, SectionsFourPeat, SEC2015, Hugs, Alexandria, SectionsTeam];
+const imageList = [
+    Rosemount2015,
+    SectionsFourPeat,
+    SEC2015,
+    Hugs,
+    Alexandria,
+    SectionsTeam,
+]
 
 const Carousel = () => {
-  const [imageNum, setImageNum] = useState(0);
+    const [imageNum, setImageNum] = useState(0)
 
-  const imageSrcs = imageList.map((img, index) => (
-    <img
-      src={img}
-      object-fit="contain"
-      alt="team photo"
-      class={clsx("transition-opacity duration-1000 ease-in-out w-8/12 md:w-6/12", {
-        "opacity-100": index === imageNum,
-        "opacity-0": index !== imageNum,
-        invisible: index !== imageNum,
-        visible: index === imageNum,
-      })}
-    />
-  ));
+    const imageSrcs = imageList.map((img, index) => (
+        <img
+            src={img}
+            style={{ maxHeight: '600px' }}
+            object-fit="contain"
+            alt="team photo"
+            class={clsx('transition-opacity duration-1000 ease-in-out h-fit', {
+                'opacity-100': index === imageNum,
+                'opacity-0': index !== imageNum,
+                invisible: index !== imageNum,
+                visible: index === imageNum,
+            })}
+        />
+    ))
 
-  useEffect(() => {
-    const interval = setInterval(
-      () =>
-        setImageNum((currNum) => {
-          return currNum === imageSrcs.length - 1 ? 0 : currNum + 1;
-        }),
-      8000
-    );
-    return () => clearInterval(interval);
-  }, []);
+    useEffect(() => {
+        const interval = setInterval(
+            () =>
+                setImageNum((currNum) => {
+                    return currNum === imageSrcs.length - 1 ? 0 : currNum + 1
+                }),
+            8000
+        )
+        return () => clearInterval(interval)
+    }, [])
 
-  return (
-    // <table class="relative w-full mb-4">
-    //   <tbody>
-    //     <tr>
-    //       <td>
-    <div class="relative w-full">
-      {imageSrcs.map((img, index) => (
-        <div
-          class={clsx("flex justify-center w-full absolute top-0 left-0", {
-            relative: index === 0,
-            absolute: index !== 0,
-          })}
-        >
-          {img}
+    return (
+        <div class="relative w-full">
+            {imageSrcs.map((img, index) => (
+                <div
+                    class={clsx(
+                        'flex justify-center w-full absolute top-0 left-0',
+                        {
+                            relative: index === 0,
+                            absolute: index !== 0,
+                        }
+                    )}
+                >
+                    {img}
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-    //       </td>
-    //     </tr>
-    //   </tbody>
-    // </table>
-  );
-};
+    )
+}
 
-export default Carousel;
+export default Carousel
--- src/components/Taskbar/Taskbar.tsx
+++ src/components/Taskbar/Taskbar.tsx
@@ -1,49 +1,58 @@
-import MediaDropdown from "../Dropdown/Dropdown";
-import { useState } from "preact/hooks";
+import MediaDropdown from '../Dropdown/Dropdown'
+import { useState } from 'preact/hooks'
 
 const Taskbar = () => {
-  const [showNav, setShowNav] = useState(false);
+    const [showNav, setShowNav] = useState(false)
 
-  const clickHandler = () => {
-    setShowNav((prevValue) => !prevValue);
-  };
+    const clickHandler = () => {
+        setShowNav((prevValue) => !prevValue)
+    }
 
-  return (
-    <>
-      <div
-        id="full width container"
-        onClick={clickHandler}
-        style={{ bottom: "-5" }}
-        class="flex md:hidden fixed bottom-0 right-0 left-0 w-screen bg-neutral-700 justify-center shadow shadow-slate-400"
-      >
-        <div
-          id="toggle-component"
-          class="flex flex-col h-12 md:hidden w-12 sticky object-bottom justify-center items-center py-0.5"
-        >
-          <div class="flex h-1 w-full my-1 bg-white">&nbsp;</div>
-          <div class="flex h-1 w-full my-1 bg-white">&nbsp;</div>
-          <div class="flex h-1 w-full my-1 bg-white">&nbsp;</div>
-        </div>
-      </div>
-      {showNav && (
-        <div class="flex flex-col fixed bottom-0 mb-12 py-4 transition-all h-fit w-full bg-white shadow shadow-slate-400">
-          <a href="/MoundsViewXC/" class="z-10 m-4 text-gray-500 hover:text-black">
-            HOME
-          </a>
-          <a href="/MoundsViewXC/schedule" class="z-10 m-4 text-gray-500 hover:text-black">
-            SCHEDULE
-          </a>
-          <a href="/MoundsViewXC/results" class="z-10 m-4 text-gray-500 hover:text-black">
-            RESULTS
-          </a>
-          <a href="/MoundsViewXC/coaches" class="z-10 m-4 text-gray-500 hover:text-black">
-            COACHES
-          </a>
-          <MediaDropdown />
-        </div>
-      )}
-    </>
-  );
-};
+    return (
+        <>
+            <div
+                id="full width container"
+                onClick={clickHandler}
+                style={{ bottom: '-5' }}
+                class="flex md:hidden fixed bottom-0 right-0 left-0 w-screen bg-neutral-700 justify-center shadow shadow-slate-400"
+            >
+                <div
+                    id="toggle-component"
+                    class="flex flex-col h-12 md:hidden w-12 sticky object-bottom justify-center items-center py-0.5"
+                >
+                    <div class="flex h-1 w-full my-1 bg-white">&nbsp;</div>
+                    <div class="flex h-1 w-full my-1 bg-white">&nbsp;</div>
+                    <div class="flex h-1 w-full my-1 bg-white">&nbsp;</div>
+                </div>
+            </div>
+            {showNav && (
+                <div class="flex flex-col fixed bottom-0 mb-12 py-4 transition-all h-fit w-full bg-white shadow shadow-slate-400">
+                    <a href="/" class="z-10 m-4 text-gray-500 hover:text-black">
+                        HOME
+                    </a>
+                    <a
+                        href="/schedule"
+                        class="z-10 m-4 text-gray-500 hover:text-black"
+                    >
+                        SCHEDULE
+                    </a>
+                    <a
+                        href="/results"
+                        class="z-10 m-4 text-gray-500 hover:text-black"
+                    >
+                        RESULTS
+                    </a>
+                    <a
+                        href="/coaches"
+                        class="z-10 m-4 text-gray-500 hover:text-black"
+                    >
+                        COACHES
+                    </a>
+                    <MediaDropdown />
+                </div>
+            )}
+        </>
+    )
+}
 
-export default Taskbar;
+export default Taskbar
