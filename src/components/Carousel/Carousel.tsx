ab56d2ade49c89e358ce139eca9e48f933bb3b88
thompsonaaron <59658614+thompsonaaron@users.noreply.github.com>
2023-04-11 11:48:36 -0500

    small, medium large images srcset

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,20 +1,52 @@
 import { useEffect, useState } from 'preact/hooks'
 
-import Alexandria from '../../assets/img-4530_orig.jpg'
-import Hugs from '../../assets/img-7128_orig.jpg'
-import Rosemount2015 from '../../assets/img-2015-rosemount-start.jpg'
-import SEC2015 from '../../assets/img-2015-cc-conference-walking.jpg'
-import SectionsFourPeat from '../../assets/img-8187_orig.jpg'
-import SectionsTeam from '../../assets/img-3485_orig.jpg'
+import AlexandriaPodiumSmall from '../../assets/alexandria-win-small.webp'
+import AlexandriaPodiumMedium from '../../assets/alexandria-win-medium.webp'
+import AlexandriaPodiumLarge from '../../assets/alexandria-win-large.webp'
+
+import HerbertHugsSmall from '../../assets/herbert-hugs-small.webp'
+import HerbertHugsMedium from '../../assets/herbert-hugs-medium.webp'
+import HerbertHugsLarge from '../../assets/herbert-hugs-large.webp'
+
+import Rosemount2015Small from '../../assets/rosemount-start-small.webp'
+import Rosemount2015Medium from '../../assets/rosemount-start-medium.webp'
+import Rosemount2015Large from '../../assets/rosemount-start-large.webp'
+
+import SEC2015Small from '../../assets/2015-cc-conference-walking-small.webp'
+import SEC2015Medium from '../../assets/2015-cc-conference-walking-medium.webp'
+import SEC2015Large from '../../assets/2015-cc-conference-walking-large.webp'
+
+import FourPeatSmall from '../../assets/four-peat-small.webp'
+import FourPeatMedium from '../../assets/four-peat-medium.webp'
+import FourPeatLarge from '../../assets/four-peat-large.webp'
+
+import ThreePeatSmall from '../../assets/three-peat-small.webp'
+import ThreePeatMedium from '../../assets/three-peat-medium.webp'
+import ThreePeatLarge from '../../assets/three-peat-large.webp'
+
 import clsx from 'clsx'
 
+// note missing rosemoutn tshirts, five-peat files
+
 const imageList = [
-    Rosemount2015,
-    SectionsFourPeat,
-    SEC2015,
-    Hugs,
-    Alexandria,
-    SectionsTeam,
+    {
+        small: Rosemount2015Small,
+        medium: Rosemount2015Medium,
+        large: Rosemount2015Large,
+    },
+    { small: FourPeatSmall, medium: FourPeatMedium, large: FourPeatLarge },
+    { small: SEC2015Small, medium: SEC2015Medium, large: SEC2015Large },
+    {
+        small: HerbertHugsSmall,
+        medium: HerbertHugsMedium,
+        large: HerbertHugsLarge,
+    },
+    {
+        small: AlexandriaPodiumSmall,
+        medium: AlexandriaPodiumMedium,
+        large: AlexandriaPodiumLarge,
+    },
+    { small: ThreePeatSmall, medium: ThreePeatMedium, large: ThreePeatLarge },
 ]
 
 const Carousel = () => {
@@ -22,8 +54,9 @@ const Carousel = () => {
 
     const imageSrcs = imageList.map((img, index) => (
         <img
-            src={img}
-            // style={{ maxWidth: '50%' }}
+            srcset={`${img.small} 300w, ${img.medium} 800w, ${img.large} 1200w`}
+            sizes="(max-width: 400px) 300px, (max-width: 900px) 800px, 1200px"
+            src={img.medium}
             object-fit="contain"
             alt="team photo"
             class={clsx(
