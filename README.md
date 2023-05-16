16239148e9b35b37d0499630d8ca008d01b9c388
thompsonaaron <59658614+thompsonaaron@users.noreply.github.com>
2023-05-16 10:52:29 -0500

    removes fontaine config and fonts

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # MoundsViewXC
+
+Google API: this was an option to allow others to post updates
+
+# Fontaine vs fontsource
+
+Both appear to have minimal content layout shift with Montserrat and Quattrocento Sans. Fontsource downloads fonts from npm via package.json ("@fontsource/montserrat": "^4.5.14", "@fontsource/quattrocento-sans": "^4.5.10") whereas fontaine requires that you download fonts directly. Both work and reduce an extra XHR request.
--- astro.config.mjs
+++ astro.config.mjs
@@ -8,12 +8,12 @@ import tailwind from '@astrojs/tailwind'
 export default defineConfig({
     integrations: [preact(), tailwind()],
     site: 'https://thompsonaaron.github.io',
-    vite: {
-        plugins: [
-            FontaineTransform.vite({
-                fallbacks: ['Arial'],
-                resolvePath: (id) => new URL(`./public${id}`, import.meta.url), // id is the font src value in the CSS
-            }),
-        ],
-    },
+    // vite: {
+    //     plugins: [
+    //         FontaineTransform.vite({
+    //             fallbacks: ['Arial'],
+    //             resolvePath: (id) => new URL(`./public${id}`, import.meta.url), // id is the font src value in the CSS
+    //         }),
+    //     ],
+    // },
 })
