93e7341c13efbcd1b450eb70b9512c1bb164570e
thompsonaaron <59658614+thompsonaaron@users.noreply.github.com>
2023-05-16 10:14:18 -0500

    adds banner.module.css

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -3,9 +3,14 @@ const BannerMedium = './images/banner-medium.webp'
 const BannerLarge = './images/banner-large.webp'
 const BannerXLarge = './images/banner-xlarge.webp'
 
+import styles from './Banner.module.css'
+
 const Banner = () => {
     return (
-        <div class="relative w-screen">
+        <div
+            class={`${styles.bannerContainer} relative h-24 md:h-fit w-full`}
+            // style={{ minHeight: '96px' }}
+        >
             <img
                 srcset={`${BannerSmall} 400w, ${BannerMedium} 800w, ${BannerLarge} 1200w, ${BannerXLarge} 1600w`}
                 sizes="(max-width: 401px) 400px, (max-width: 801px) 800px, (max-width: 1201px): 1200px, 1600px"
@@ -13,6 +18,7 @@ const Banner = () => {
                 object-fit="cover"
                 alt="homepage-background"
                 width="100%"
+                // height="400px"
                 style={{ maxHeight: '400px' }}
             />
             <h1 class="absolute text-xs md:text-xl right-10 bottom-4 md:bottom-6 font-bold">
